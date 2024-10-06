7ec441dce84a18a116b56b238ccd74c299cf479e
Carlos Zamora <cdzamora02@outlook.es>
2024-10-06 13:26:26 -0600

    Add docs to file

--- curso/code/OpenMP/hello.c
+++ curso/code/OpenMP/hello.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+/*
+Comando para generar el ejecutable:
+
+gcc hello.c -o hello -fopenmp
+
+Salida:
+
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+Hola Mundo
+
+*/
+
+
 int main(){
 
     #pragma omp parallel
