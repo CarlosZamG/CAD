549a54d3a3b4a552ec5dfc03fcd2df18bd5c5c63
Carlos Zamora <cdzamora02@outlook.es>
2024-10-06 13:35:08 -0600

    Add docs to memory_01.c

--- curso/code/others/memory_01.c
+++ curso/code/others/memory_01.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Comando para generar el ejecutable:
+
+gcc memory_01.c -o memory_01
+
+Salida:
+
+Dirección del código: 0x5650d8051189
+Dirección del Stack: 0x7fffa823df54
+Dirección del Heap: 0x5650d95296b0
+
+*/
+
+
 int main(){
 
     printf("Dirección del código: %p\n", main);
