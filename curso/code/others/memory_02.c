ffebf63063233de7276c92c4f85b3595716ce170
Carlos Zamora <cdzamora02@outlook.es>
2024-10-06 18:01:01 -0600

    Add docs to memory_02.c

--- curso/code/others/memory_02.c
+++ curso/code/others/memory_02.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Comando para generar el ejecutable:
+
+gcc memory_02.c -o memory_02
+
+Salida:
+
+Valor de n: 21893. Dirección de n: 0x7ffdc16e2eec
+Valor de pn: 0x7ffdc16e2ff0. Dirección de pn: 0x7ffdc16e2ef0
+
+Valor de n: 5. Dirección de n: 0x7ffdc16e2eec
+Valor de pn: 0x7ffdc16e2eec. Dirección de pn: 0x7ffdc16e2ef0
+
+Valor de n: 12. Dirección de n: 0x7ffdc16e2eec
+Valor de pn: 0x55850fb306b0. Dirección de pn: 0x7ffdc16e2ef0
+
+
+*/
+
+
 int main(){
 
     int n;
