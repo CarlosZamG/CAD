e9b69256d47eb0208c54c186d165b29caa608099
Carlos Zamora <cdzamora02@outlook.es>
2024-10-06 19:35:02 -0600

    Improve printing

--- curso/code/others/pointers_03.c
+++ curso/code/others/pointers_03.c
@@ -13,14 +13,15 @@ int main(){
     printf("\n");
 
     printf("La variable q2 con valor: %p vive en %p y el contenido (como entero) de la dirección que guarda es %d\n", q2, &q2, *q2);
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, *q2);
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, ((void *) ((unsigned int) *q2)));
     
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, ((void *) *q2));
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, ((int *) *q2));
+    printf("La variable q2 con valor: %p vive en %p y el contenido (como puntero) de la dirección que guarda es %p\n", q2, &q2, *q2);
+    printf("La variable q2 con valor: %p vive en %p y el contenido en la forma ((void *) ((unsigned int) *q2)) de la dirección que guarda es %p\n", q2, &q2, ((void *) ((unsigned int) *q2)));
+    
+    printf("La variable q2 con valor: %p vive en %p y el contenido en la forma ((void *) *q2) de la dirección que guarda es %p\n", q2, &q2, ((void *) *q2));
+    printf("La variable q2 con valor: %p vive en %p y el contenido en la forma ((int *) *q2) de la dirección que guarda es %p\n", q2, &q2, ((int *) *q2));
 
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, *((void **)q2));
-    printf("La variable q2 con valor: %p vive en %p y el contenido de la dirección que guarda es %p\n", q2, &q2, *((int **)q2));
+    printf("La variable q2 con valor: %p vive en %p y el contenido en la forma *((void **)q2) de la dirección que guarda es %p\n", q2, &q2, *((void **)q2));
+    printf("La variable q2 con valor: %p vive en %p y el contenido en la forma *((int **)q2) de la dirección que guarda es %p\n", q2, &q2, *((int **)q2));
     
     printf("%d - (%d) = %d\n", *q2, *q2, *q2-*q2);
 
