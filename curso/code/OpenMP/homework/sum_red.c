a2c21e5e4c9be92f3bf16cdbca4a95e174cb4364
Carlos Zamora <cdzamora02@outlook.es>
2024-10-19 17:51:05 -0600

    Add code for homework to sum doubles

--- curso/code/OpenMP/homework/sum_red.c
+++ curso/code/OpenMP/homework/sum_red.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define SIZE 1000000
+#define SIZE 10000
 
 int main()
 {
     double arr[SIZE];
+
     double parallel_sum = 0.0;
     double serial_sum = 0.0;
-    double l1 = -2220.0;
-    double l2 = 11230.0;
 
+    double l1 = -100000.0;
+    double l2 = 120000.0;
 
     for (int i = 0; i < SIZE; i++)
     {
         arr[i] = l1 + (l2-l1)*rand()/RAND_MAX;
-        //printf("%d %lf\n", i, arr[i]);
     }
     
 
@@ -25,7 +25,6 @@ int main()
     {
         parallel_sum += arr[i];
     }
-
 
     printf("Suma de los elementos del arreglo en paralelo: %lf\n", parallel_sum);
 
