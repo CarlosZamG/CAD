9e4140c88a32ab84cb4bc12498bf3c6b501442ad
Carlos Zamora <cdzamora02@outlook.es>
2024-10-29 09:41:41 -0600

    WIP: check barrier

--- curso/code/MPI/homework/ex_02.c
+++ curso/code/MPI/homework/ex_02.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 int main(int argc, char** argv){
@@ -8,12 +9,17 @@ int main(int argc, char** argv){
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
+    double start = MPI_Wtime();
+    double time;
+
     for(int i = 0; i < size_Of_Cluster; i++){
         if(i == process_Rank){
-            printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
+            time = MPI_Wtime() - start;
         }
         MPI_Barrier(MPI_COMM_WORLD);
     }
+
+    printf("\nTime %d: %lf ", process_Rank, time);
 
     MPI_Finalize();
     return 0;
